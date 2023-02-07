b837ebb8df2c9d2f8e8ae5a8825dc01ab5c19394
Muhammad Awais Butt <awaisbutt075@gmail.com>
2023-02-07 17:46:39 +0500

    Add files via upload

---
+++ Assignments - Amir Qayyum/JS/2_ PF-1_ Assignments/stringReverse.js
@@ -0,0 +1,8 @@
+function string_reverse(str) 
+{
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("Muhammad"));
+console.log(string_reverse("Awais"));
+console.log(string_reverse("Butt"));
---
+++ Assignments - Amir Qayyum/JS/2_JS-1_ Assignments/triangleArea.js
@@ -0,0 +1,6 @@
+var side1 = 2; 
+var side2 = 3; 
+var side3 = 4; 
+var s = (side1 + side2 + side3)/2;
+var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+console.log(area);
